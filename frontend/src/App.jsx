9119122ce0289bee2d88cc22d0eedeb82a146b1d
dcho-jaewook <fermatjw@gmail.com>
2025-02-09 10:31:31 -0800

    Single Post Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,15 @@ import './App.css'
 import UserIcon from './components/User/UserIcon'
 import PostHeader from './components/Post/PostHeader'
 import Post from './components/Post/Post'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Post/>
+    <Router>
+      <Routes>
+        <Route path="/post/:post_id" element={<Post />}></Route>
+      </Routes>
+    </Router>
   )
 }
 
