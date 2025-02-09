3d9ed27247e9808f7f827a131026fbf5af022104
Jusung Kim <itmekjs@gmail.com>
2025-02-09 15:06:29 -0700

    App Routing Home UI/Conflict resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import UserIcon from './components/User/UserIcon'
-import PostHeader from './components/Post/PostHeader'
 import Post from './components/Post/Post'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 //
+import Home from './components/Pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/post/:post_id" element={<Post />}></Route>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:post_id" element={<Post />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
