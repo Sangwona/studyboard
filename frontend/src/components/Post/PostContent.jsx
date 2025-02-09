c39c6bdbdcdc5b5c5889224cdbc49330373b6ab9
dcho-jaewook <fermatjw@gmail.com>
2025-02-08 20:50:29 -0800

    Getting post information from the published backend

--- frontend/src/components/Post/PostContent.jsx
+++ frontend/src/components/Post/PostContent.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const PostContext = ({ content }) => {
     return (
-        // <p>{content}</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt veniam unde saepe molestias perferendis illum, illo earum commodi voluptatum, magni quibusdam pariatur quis id. Voluptatem, tenetur praesentium. Vero, vel dolore.</p>
+        <p>{content}</p>
+        // <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt veniam unde saepe molestias perferendis illum, illo earum commodi voluptatum, magni quibusdam pariatur quis id. Voluptatem, tenetur praesentium. Vero, vel dolore.</p>
     )
 }
 
--- frontend/src/components/Post/PostHeader.jsx
+++ frontend/src/components/Post/PostHeader.jsx
@@ -7,12 +7,12 @@ const PostHeader = ({ title, author, date }) => {
     return (
         <div id="post-header">
             <div className='post-wrapper'>
-                {/* <h1>{title}</h1> */}
-                <h1 className='post-header-title'>This is a Cool Post about the StudyBoard</h1>
+                <h1>{title}</h1>
+                {/* <h1 className='post-header-title'>This is a Cool Post about the StudyBoard</h1> */}
                 <div className='post-header-author'>
                     <UserIcon></UserIcon>
                     {/* <p>{date.year}-{date.month}-{date.day}</p> */}
-                    <p className='post-header-date'>· 2025-02-06</p>
+                    {/* <p className='post-header-date'>· 2025-02-06</p> */}
                 </div>
             </div>
         </div>
