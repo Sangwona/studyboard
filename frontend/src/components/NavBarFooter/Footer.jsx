df5b504e9264325198d6de44feb30976a4451212
Jusung Kim <itmekjs@gmail.com>
2025-02-10 15:51:09 -0700

    Dividing  Navbar from Home

---
+++ frontend/src/components/NavBarFooter/Footer.jsx
@@ -0,0 +1,11 @@
+import "../../styles/Footer.css"; // 푸터 스타일
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 My Board. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
