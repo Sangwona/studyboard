7ef7fb1d1bfebb48b11cadde67363f09df4534bc
TripToCodes <iam.somj@gmail.com>
2025-03-18 22:09:53 -0700

    Add PropTypes and remove React importation

--- frontend/src/components/Pages/WriteForm.jsx
+++ frontend/src/components/Pages/WriteForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "../../styles/WriteForm.css";
 import { useNavigate } from "react-router-dom";
 
