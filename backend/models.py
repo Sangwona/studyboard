c2a35eed3a409a36d85088c9f865a688089f3bc8
Priscilla <166824855+Priscilla2424@users.noreply.github.com>
2025-03-22 08:50:16 -0400

    new created table pk  change

--- backend/models.py
+++ backend/models.py
@@ -4,12 +4,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import Sequence, text
 
 
-# 시퀀스 객체 생성
-role_id_seq = Sequence('role_id_seq', start=1)
-group_id_seq = Sequence('group_id_seq', start=1)
-
-
-# id 지우고user_id 를 프라이머리키로 바꿔야함!
 # 사용자 (User) 테이블
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -63,8 +57,7 @@ class Comment(db.Model):
 class Role(db.Model):
     __tablename__ = 'roles'
 
-    id = db.Column(db.String, primary_key=True, server_default=text(
-        "'role_' || nextval('role_id_seq')"))
+    id = db.Column(db.Integer, primary_key=True)
     role_name = db.Column(db.String, nullable=False)
 
     def __repr__(self):
@@ -76,9 +69,9 @@ class Role(db.Model):
 class UserRole(db.Model):
     __tablename__ = 'user_roles'
 
-    user_id = db.Column(db.String, db.ForeignKey(
-        "user.user_id", ondelete="CASCADE"), primary_key=True)
-    role_id = db.Column(db.String, db.ForeignKey(
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        "user.id", ondelete="CASCADE"), primary_key=True)
+    role_id = db.Column(db.Integer, db.ForeignKey(
         "roles.id", ondelete="CASCADE"), primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -91,13 +84,12 @@ class UserRole(db.Model):
 class Group(db.Model):
     __tablename__ = 'groups'
 
-    id = db.Column(db.String, primary_key=True, server_default=text(
-        "'group_' || nextval('group_id_seq')"))
+    id = db.Column(db.Integer, primary_key=True)
     group_name = db.Column(db.String, unique=True, nullable=False)
     max_members = db.Column(db.Integer, default=100)
     is_public = db.Column(db.Boolean, default=True)
-    manager_id = db.Column(db.String, db.ForeignKey(
-        "user.user_id", ondelete="SET NULL"))
+    manager_id = db.Column(db.Integer, db.ForeignKey(
+        "user.id", ondelete="SET NULL"))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     description = db.Column(db.Text)
 
@@ -110,10 +102,10 @@ class Group(db.Model):
 class GroupMember(db.Model):
     __tablename__ = 'group_members'
 
-    group_id = db.Column(db.String, db.ForeignKey(
+    group_id = db.Column(db.Integer, db.ForeignKey(
         "groups.id", ondelete="CASCADE"), primary_key=True)
-    user_id = db.Column(db.String, db.ForeignKey(
-        "user.user_id", ondelete="CASCADE"), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        "user.id", ondelete="CASCADE"), primary_key=True)
     status = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
