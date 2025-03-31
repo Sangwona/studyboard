131d2b2e9ffd86f119cbb2301ebf95d70e2e16d1
TripToCodes <iam.somj@gmail.com>
2025-03-30 17:37:47 -0700

    Display Edit button only to comment author

--- frontend/src/components/Comment/Comment.jsx
+++ frontend/src/components/Comment/Comment.jsx
@@ -1,15 +1,33 @@
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import moment from "moment-timezone";
 
 const Comment = ({ comment }) => {
+  const [isCommentAuthor, setIsCommentAuthor] = useState(false);
+  const userId = parseInt(localStorage.getItem("user_id"));
+
+  useEffect(() => {
+    setIsCommentAuthor(comment.user_id === userId);
+  }, [comment.user_id, userId]);
+
   return (
     <div className="comment">
       <div className="comment-content">{comment.content}</div>
       <div className="comment-meta">
-        <span className="comment-author">{comment.username}</span>
-        <span className="comment-date">
-          {moment.utc(comment.created_at).tz(moment.tz.guess()).format("MM-DD-YYYY, HH:mm:ss")}
-        </span>
+        <div className="comment-author">{comment.username}</div>
+        <div className="comment-actions-date-wrapper">
+          {isCommentAuthor ? (
+            <div className="comment-actions-wrapper">
+              <span>Edit</span>
+              <span>Delete</span>
+            </div>
+          ) : (
+            <div className="comment-actions-wrapper"></div>
+          )}
+          <span className="comment-date">
+            {moment.utc(comment.created_at).tz(moment.tz.guess()).format("MM-DD-YYYY, HH:mm:ss")}
+          </span>
+        </div>
       </div>
     </div>
   );
@@ -17,10 +35,11 @@ const Comment = ({ comment }) => {
 
 Comment.propTypes = {
   comment: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     content: PropTypes.string.isRequired,
+    created_at: PropTypes.string.isRequired,
     user_id: PropTypes.number.isRequired,
     username: PropTypes.string.isRequired,
-    created_at: PropTypes.string.isRequired,
   }).isRequired,
 };
 
